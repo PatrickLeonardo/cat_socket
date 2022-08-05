42a354a93563771af9348ebeb6f1fd7f789f883f
PatrickLeonardo <ticklf7@gmail.com>
2022-08-04 21:45:26 -0300

    file

---
+++ client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 3001
+#define BUFFER_LENGTH 4096
+#define SERVER_ADDR "127.0.0.1"
+
+int main(void)
+{
+    struct sockaddr_in server;
+
+    int sockfd;
+
+    char buffer_in[BUFFER_LENGTH];
+    char buffer_out[BUFFER_LENGTH];
+    
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == !0)
+    {
+        perror("\nErro ao criar o socket do cliente: ");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nCliente conectado!\n");
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+    int server_len = sizeof(server);
+    if(connect(sockfd, (struct sockaddr*) &server, server_len) == !0)
+    {
+        perror("\nErro ao se conectar ao servidor: ");
+        exit(EXIT_FAILURE);
+    }
+    printf("Conexão com o servidor estabelecida!\n");
+
+    if(recv(sockfd, buffer_in, BUFFER_LENGTH, 0) > 0)
+    {
+        printf("\n%s",buffer_in);
+    }
+
+    do
+    {
+        memset(buffer_in, 0, BUFFER_LENGTH);
+        memset(buffer_out, 0, BUFFER_LENGTH);
+
+        printf("Insira o nome de algum arquivo: ");
+        fgets(buffer_out, BUFFER_LENGTH, stdin);
+
+        send(sockfd, buffer_out, strlen(buffer_out), 0);
+        recv(sockfd, buffer_in, BUFFER_LENGTH, 0);
+        printf("\n%s\n\n", buffer_in);
+
+    } while (strcmp(buffer_in, "Volte sempre!"));
+    
+    close(sockfd);
+    printf("Conexão encerrada!\n\n");
+    return(EXIT_SUCCESS);
+
+}
---
+++ server.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define BUFFER_LENGTH 4096
+#define PORT 3001
+
+int main(void)
+{
+    struct sockaddr_in server, client;
+
+    int serverfd, clientfd;
+
+    char buffer[BUFFER_LENGTH];
+
+    if((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == !0)
+    {
+        perror("\nErro ao criar o servidor: ");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nServidor conectado!\n");
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+ 
+    int value = 1;
+    if(setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value)) == !0)
+    {
+        perror("\nErro ao redirecionar as opções: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(bind(serverfd, (struct sockaddr*) &server, sizeof(server)) == !0)
+    {
+        perror("\nErro no bind: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(serverfd, 1) == !0)
+    {
+        perror("\nErro na listagem da porta: ");
+        exit(EXIT_FAILURE);
+    }
+    printf("Listando na porta: %i!\n", PORT);
+
+    socklen_t client_len = sizeof(client);
+    if((clientfd = accept(serverfd, (struct sockaddr*) &client, &client_len)) == !0)
+    {
+        perror("\nErro no accept: ");
+        exit(EXIT_FAILURE);
+    }
+    
+    strcpy(buffer, "Seja bem vindo! (digite 'exit' para sair)\n");
+
+    if(send(clientfd, buffer, strlen(buffer), 0))
+    {
+        printf("\nCliente conectado.\nEsperando por alguma mensagem ...\n\n");
+        
+        do
+        {
+            memset(buffer, 0, BUFFER_LENGTH);
+
+            int message_len;
+            if((message_len = recv(clientfd, buffer, BUFFER_LENGTH, 0)) > 0)
+            {
+                buffer[message_len - 1] = '\0';
+                printf("Cliente diz: %s\n", buffer);
+            }
+
+            if(strstr(buffer, ".txt"))    
+            {
+                FILE *file;
+                file = fopen(buffer, "r");
+                char get[BUFFER_LENGTH];
+                char content[BUFFER_LENGTH];
+                while (!feof(file))
+                {
+                    fgets(get, BUFFER_LENGTH, file);
+                    strcat(content, get);
+                }
+                if(send(clientfd, content, BUFFER_LENGTH, 0));
+                {
+                    printf("Arquivo enviado!\n\n");
+                }
+            }
+
+            if(strcmp(buffer, "exit") == 0)
+            {
+                send(clientfd, "Volte sempre!", 13, 0);
+            }
+
+        } while (strcmp(buffer, "exit"));
+    }
+    
+    close(serverfd);
+    printf("\nConexão encerrada!\n\n");
+    return(EXIT_SUCCESS);
+  
+}
